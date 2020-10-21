bb1454659a684100724e562aeec84388ed47f735
JassoEC <isw.ecamposj@gmail.com>
2020-10-21 16:15:54 -0500

    finalización de aplicación de Contador, botones de agregar, restar y reset a 0

--- lib/src/pages/counter_page.dart
+++ lib/src/pages/counter_page.dart
@@ -14,26 +14,39 @@ class _CounterPageState extends State<CounterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Contador'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Numero de Tabs', style: _textStyle),
-            Text('$_counter', style: _textStyle)
-          ],
+        appBar: AppBar(
+          title: Text('Contador'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _counter++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Numero de Tabs', style: _textStyle),
+              Text('$_counter', style: _textStyle)
+            ],
+          ),
+        ),
+        floatingActionButton: _createButtons());
   }
+
+  Widget _createButtons() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          FloatingActionButton(
+              onPressed: _reset, child: Icon(Icons.exposure_zero)),
+          FloatingActionButton(onPressed: _dismiss, child: Icon(Icons.remove)),
+          FloatingActionButton(onPressed: _add, child: Icon(Icons.add)),
+        ]);
+  }
+
+  void _add() => setState(() => _counter++);
+
+  void _dismiss() {
+    if (_counter > 0) {
+      setState(() => _counter--);
+    }
+  }
+
+  void _reset() => setState(() => _counter = 0);
 }
